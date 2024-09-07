c3991a5cc9d29c6f994f6b33565554410c726ff7
The-0mnipotent <arpit84_soe@jnu.ac.in>
2024-09-07 19:01:20 +0530

    fixing build

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -2,7 +2,7 @@
 import React from "react";
 import Footer from "../components/Footer";
 import Header from "../components/Header";
-import HeroSection from "../components/Herosection";
+import HeroSection from "../components/HeroSection";
 
 const HomePage = ({ products }) => {
   return (
