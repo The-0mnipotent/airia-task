0f07835fe6a6eb99d996ee3577f82288444c2ef1
The-0mnipotent <arpit84_soe@jnu.ac.in>
2024-09-07 19:31:24 +0530

    cors issue

--- vite.config.js
+++ vite.config.js
@@ -1,23 +1,23 @@
-// import react from "@vitejs/plugin-react";
-// import { defineConfig } from "vite";
-
-// // https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// });
-
 import react from "@vitejs/plugin-react";
 import { defineConfig } from "vite";
 
+// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://cdn.drcode.ai",
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, ""),
-      },
-    },
-  },
 });
+
+// import react from "@vitejs/plugin-react";
+// import { defineConfig } from "vite";
+
+// export default defineConfig({
+//   plugins: [react()],
+//   server: {
+//     proxy: {
+//       "/api": {
+//         target: "https://cdn.drcode.ai",
+//         changeOrigin: true,
+//         rewrite: (path) => path.replace(/^\/api/, ""),
+//       },
+//     },
+//   },
+// });
